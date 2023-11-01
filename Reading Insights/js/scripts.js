8e0fc82667b88bd9924e560b3468f1d0c43872e7
MarkZhang2023 <145871603+MarkZhang2023@users.noreply.github.com>
2023-10-31 20:38:24 -0400

    Add files via upload

--- Reading Insights/js/scripts.js
+++ Reading Insights/js/scripts.js
@@ -5,3 +5,10 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+function searchFunction(event) {
+event.preventDefault();
+const searchTerm = document.getElementById('search').value;
+// You can perform further actions with the search term, like sending it to a server or processing it locally.
+console.log('Search term:', searchTerm);
+}
